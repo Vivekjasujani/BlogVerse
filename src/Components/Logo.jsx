213751daf0fc219d9133413c6fdc1e5031ed4067
Vivekjasujani <vivekjasujani987@gmail.com>
2024-08-12 01:15:10 +0530

    project dependencies added

---
+++ src/Components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Logo() {
+  return (
+    <div>
+      <h1 className='font-bold text-[#605BFF] text-lg'>BlogVerse</h1>
+    </div>
+  );
+}
+
+export default Logo;
